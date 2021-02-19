d92b51c7f232f7ea626de7adb30ffbe1dbb8cd5b
J Edwards <jesse.edwards2@protonmail.com>
2021-02-19 15:41:53 +0000

    added start positions, testing character movement

--- scripts/app.js
+++ scripts/app.js
@@ -9,7 +9,12 @@ const cellCount = width * height
 const cells = []
 
 const playerStartingPosition = 94
-let currentCatPosition = 0
+let playerCurrentPosition = 94
+const playerClass = 'playerCharacter'
+
+const enemyStartingPosition = 34
+let enemyCurrentPosition = 0
+const enemyClass = 'enemyCharacter'
 //! **************GAME BOARD**************************
 function createGrid() {
   for (let i = 0; i < cellCount; i++) {
@@ -18,13 +23,39 @@ function createGrid() {
   grid.appendChild(cell)
   cells.push(cell)
   }
+addStartingPositions(playerStartingPosition, playerClass)
+addStartingPositions(enemyStartingPosition, enemyClass)
 }
+console.log(cells)
 createGrid()
 
-function addCharacterToBoard() {
-  cells[playerStartingPosition].classlist.add('playerCharacter')
+function addStartingPositions(position, characterType) {
+  cells[position].classList.add([characterType])
 }
-addCharacterToBoard()
+
+function removeCharacter(position, character) {
+  cells[position].classList.remove(character)
+}
+
+
+function moveCharacter(keyPress) {
+  const key = keyPress.keyCode
+  removeCharacter(playerCurrentPosition, playerClass)
+  if (key === 39 || key === 68) {
+    playerCurrentPosition++
+    console.log(playerCurrentPosition)
+  } else if (key === 37 || key === 18) {
+    playerCurrentPosition--
+  } 
+
+}
+
+
+
+
+
+document.addEventListener('keydown', moveCharacter)
+
 
 
 
