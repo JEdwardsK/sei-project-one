559a5344e018cc3158df9e4811ddd91ad22b9cf4
J Edwards <jesse.edwards2@protonmail.com>
2021-02-19 14:33:36 +0000

    grid creation testing

--- scripts/app.js
+++ scripts/app.js
@@ -1,9 +1,23 @@
 function init() {
   console.log ('content loaded')
 
+// Variables ***************************** //
 
+const grid = document.querySelector('.grid')
+const width = 10
+const height = 10
+const cellCount = width * height
+const cells = []
 
+function createGrid() {
+  for (let i = 0; i <= cellCount; i++) {
+  const cell = document.createElement('div')
+  cell.innerHTML = i
+  grid.appendChild(cell)
+  }
+}
 
+createGrid()
 
 
 
