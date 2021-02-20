e9cd60f58fd4d502a1231dcdc7c611f27d6163ca
J Edwards <jesse.edwards2@protonmail.com>
2021-02-20 15:45:43 +0000

    function to remove all if player/enemy intersect

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,7 @@
 function init() {
   const testString = 'i am a test string to test this works'
-  // Variables ***************************** //
+
+  //! ***************************** Variables ***************************** 
 
   const grid = document.querySelector('.grid')
   const width = 10
@@ -12,16 +13,15 @@ function init() {
   let playerCurrentPosition = playerStartingPosition
   const playerClass = 'playerCharacter'
 
-  const weaponStartingPosition = 0
+  const weaponStartingPosition = playerCurrentPosition
   let weaponCurrentPosition 
   const weaponClassBolt = 'weaponBolt'
 
-  const enemyStartingPosition = 30
+  const enemyStartingPosition = 94
   let enemyCurrentPosition = enemyStartingPosition
   const enemyClass = 'enemyCharacter'
 
-
-  //! **************GAME BOARD**************************
+  //! **********************GAME START FUNCTIONS*****************************
   function createGrid() {
     for (let i = 0; i < cellCount; i++) {
     const cell = document.createElement('div')
@@ -42,65 +42,74 @@ function init() {
     cells[position].classList.remove(character)
   }
 
+  function characterMovement(key) {
+    removeCharacter(playerCurrentPosition, playerClass)
+    if ((key === 39 || key === 68) && playerCurrentPosition % width !== width - 1) {
+      playerCurrentPosition++
+      addCharacter(playerCurrentPosition, playerClass)
+      console.log(playerCurrentPosition)
+    } else if ((key === 37 || key === 18) && playerCurrentPosition % width !== 0) {
+      playerCurrentPosition--
+      addCharacter(playerCurrentPosition, playerClass)
+    } else{
+      addCharacter(playerCurrentPosition, playerClass)
+    } 
+  }
+
+
   function characterMoveset(keyPress) {
     const key = keyPress.keyCode
-    //character Movement
-    if (key === 39 || key === 68 || key === 37 || key === 18) {
-      removeCharacter(playerCurrentPosition, playerClass)
-      if ((key === 39 || key === 68) && playerCurrentPosition % width !== width - 1) {
-        playerCurrentPosition++
-        addCharacter(playerCurrentPosition, playerClass)
-        console.log(playerCurrentPosition)
-      } else if ((key === 37 || key === 18) && playerCurrentPosition % width !== 0) {
-        playerCurrentPosition--
-        addCharacter(playerCurrentPosition, playerClass)
-      } else{
-        addCharacter(playerCurrentPosition, playerClass)
-      }
+    if (key === 39 || key === 68 || key === 37 || key === 18){cell
+      characterMovement(key)
     } else if (key === 32){
       weaponCurrentPosition = playerCurrentPosition -= width
       addCharacter(weaponCurrentPosition, weaponClassBolt)
       playerCurrentPosition += width
       let bolt = setInterval(() => {
         removeCharacter(weaponCurrentPosition, weaponClassBolt)
-        if (weaponCurrentPosition > 0) {
+        if (weaponCurrentPosition > width) {
           weaponCurrentPosition -= width
           addCharacter(weaponCurrentPosition, weaponClassBolt)
         } else {
+          weaponCurrentPosition = weaponStartingPosition
           clearInterval(bolt)
         }        
       }, 100);
-    }
+    } 
   }
-
-
-  // let enemyMovement = setInterval(() => {
-  //   if (enemyCurrentPosition % width === width - 1) {
-  //     let enemyMovementRightAndDown =  setInterval(() => {
-  //         removeCharacter(enemyCurrentPosition, enemyClass)
-  //         if (enemyCurrentPosition % width !== width - 1){
-  //           enemyCurrentPosition++
-  //           addCharacter(enemyCurrentPosition, enemyClass)
-  //         } else {
-  //           enemyCurrentPosition += width
-  //           addCharacter(enemyCurrentPosition, enemyClass)
-  //           clearInterval(enemyMovementRightAndDown)
-  //         }
-  //       }, 1000);
-  //   } else if (enemyCurrentPosition % width === width) { 
-  //     let enemyMovementLeftAndDown = setInterval(() => {
-  //       removeCharacter(enemyCurrentPosition, enemyClass)
-  //       if (enemyCurrentPosition % width !== 0) {
-  //         enemyCurrentPosition--
-  //         addCharacter(enemyCurrentPosition, enemyClass)
-  //         clearInterval(enemyMovementLeftAndDown)
-  //       }
-  //       }
-  //     }, 1000);
-  //   }
-  // }, 10000);
+  
+  // function addEnemyRow(){
+  //   cells[34, 36].classList.add(enemyClass)
   // }
+  // addEnemyRow()
+    // const enemyMovementRightAndDown =  setInterval(() => {
+    //   removeCharacter(enemyCurrentPosition, enemyClass)
+    //   if (enemyCurrentPosition % width !== width - 1){
+    //     enemyCurrentPosition++
+    //     addCharacter(enemyCurrentPosition, enemyClass)
+    //   } else {
+    //   enemyCurrentPosition += width
+    //   addCharacter(enemyCurrentPosition, enemyClass)
+    //   clearInterval(enemyMovementRightAndDown)
+    //   }
+    // }, 1000);
+    // const enemyMovementLeftAndDown = setInterval(() => {
+    //   removeCharacter(enemyCurrentPosition, enemyClass)
+    //   if (enemyCurrentPosition % width !== 0) {
+    //     enemyCurrentPosition--
+    //     addCharacter(enemyCurrentPosition, enemyClass)
+    //     clearInterval(enemyMovementLeftAndDown)
+    //   }
+    // }, 1000);
 
+  cells.forEach(cell => {
+    let cellPosition = cells.indexOf(cell)
+  //   console.log (cells.indexOf(cell))
+    if (cells[cellPosition].classList.value === `${playerClass} ${enemyClass}`) {
+      removeCharacter(cellPosition,enemyClass)
+      removeCharacter(cellPosition,playerClass)
+    }
+  });
 document.addEventListener('keydown', characterMoveset)
 
 
